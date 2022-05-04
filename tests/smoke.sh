5e84f9543ee3c06f053a306827a366a8c2918b26
srcmaxim <mkoval@revcontent.com>
2022-05-04 18:03:39 +0400

    Update K6 quiet logging

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -2,4 +2,4 @@
 
 SCRIPTPATH=$(dirname "$0")
 docker run --rm -i --network="host" grafana/k6 \
-  run -e BASE_URL=http://127.0.0.1:8080 - <"${SCRIPTPATH}/smoke.js"
+  run -e BASE_URL=http://127.0.0.1:8080 --quiet - <"${SCRIPTPATH}/smoke.js"
